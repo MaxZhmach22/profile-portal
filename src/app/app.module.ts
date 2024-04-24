b8298f258c415d48e5be784fc1a78aced754d0f0
Max <maxzhmach@mail.ru>
2024-04-25 01:07:56 +0300

    [develop] add start unity game component / adding broti decoding plugin in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UnityGameHandlerComponent } from './unity-game-handler/unity-game-handler.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnityGameHandlerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/unity-game-handler/unity-game-handler.component.ts
@@ -0,0 +1,44 @@
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-unity-game-handler',
+  templateUrl: './unity-game-handler.component.html',
+  styleUrls: ['./unity-game-handler.component.css']
+})
+export class UnityGameHandlerComponent implements AfterViewInit{
+  ngAfterViewInit(): void {
+
+    const script = document.createElement('script');
+    script.src = "assets/unity/Build/unity.loader.js";
+    script.onload = (ev) => {
+      if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+        // Mobile device style: fill the whole browser client area with the game canvas:
+        var meta = document.createElement('meta');
+        meta.name = 'viewport';
+        meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+        document.getElementsByTagName('head')[0].appendChild(meta);
+
+        const canvas = document.querySelector("#unity-canvas") as HTMLCanvasElement;
+        canvas.style.width = "100%";
+        canvas.style.height = "100%";
+        canvas.style.position = "fixed";
+
+        document.body.style.textAlign = "left";
+      }
+
+      // @ts-ignore
+      createUnityInstance(document.querySelector("#unity-canvas"), {
+        dataUrl: "assets/unity/Build/unity.data.br",
+        frameworkUrl: "assets/unity/Build/unity.framework.js.br",
+        codeUrl: "assets/unity/Build/unity.wasm.br",
+        streamingAssetsUrl: "assets/unity/StreamingAssets",
+        companyName: "DefaultCompany",
+        productName: "dune.io",
+        productVersion: "0.1",
+        // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
+        // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
+      });
+    }
+    document.body.appendChild(script);
+  }
+}
